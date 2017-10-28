b62d44993d43a5b26f310dd5f84f5183e67f8fd6
lucas fernando <Luca-max@hotmail.com>
2017-10-28 17:55:52 -0200

    Criação dos metodos basicos para API

---
+++ DomainBusiness/PropiedadesBO.cs
@@ -0,0 +1,65 @@
+﻿using Entity;
+using wpServices;
+using System.Threading.Tasks;
+using Repository;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace DomainBusiness
+{
+    public static class PropiedadesBO
+    {
+        /// <summary>
+        /// Metodo de salvar Propiedades (Async)
+        /// </summary>
+        /// <param name="Propiedades"> Objeto Produtp</param>
+        /// <param name="token"> Token valido</param>
+        /// <returns>Verdadeiro: Salvou o Propiedades / Falso: Houve falha</returns>
+        public static async Task<bool> SaveAsync(Propiedades Propiedades, string token)
+        {
+            if (await SeguracaServ.validaTokenAsync(token))
+            {
+                if (Propiedades.idCliente != 0)
+                    try { return PropiedadesRep.Save(Propiedades); } catch { return false; }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Metodo de retornar todos os Propiedadess por cliente
+        /// </summary>
+        /// <param name="idCliente">ID do cliente solicitante</param>
+        /// <param name="token">Token Valido</param>
+        /// <returns></returns>
+        public static async Task<List<Propiedades>> GetAllAsync(int idCliente, string token)
+        {
+            if (await SeguracaServ.validaTokenAsync(token))
+                return PropiedadesRep.GetAll().Where(x => x.idCliente == idCliente).ToList();
+            else
+                return new List<Propiedades>();
+        }
+
+        /// <summary>
+        /// Metodo de deletar Propiedades
+        /// </summary>
+        /// <param name="Propiedades">Propiedades que iraser deletado</param>
+        /// <param name="token">Token valido</param>
+        /// <returns>Verdadeiro: Removeu o Propiedades / Falso: Houve falha</returns>
+        public static async Task<bool> RemoveAsync(Propiedades Propiedades, string token)
+        {
+            dynamic objEn = Propiedades;
+            string a = objEn.ID.ToString();
+            if (await SeguracaServ.validaTokenAsync(token))
+            {
+                Propiedades obj = PropiedadesRep.GetAll().Where(x => x.ID == Convert.ToInt32(a)).FirstOrDefault();
+                return PropiedadesRep.Remove(obj);
+            }
+            else
+                return false;
+        }
+    }
+}
